d1832d5f17ca41885220bded49283ebb0dc16319
JoshD564 <299042841+JoshD564@users.noreply.github.com>
2026-07-06 15:33:44 +0100

    copied spells

---
+++ HarryPotter/HarryPotter.Client/Core/Models/Spells.cs
@@ -0,0 +1,16 @@
+using System.Text.Json.Serialization;
+
+namespace HarryPotter.Server.Models
+{
+    public class Spell
+    {
+        [JsonPropertyName("id")]
+        public string? id { get; set; }
+
+        [JsonPropertyName("name")]
+        public decimal? name { get; set; }
+        
+        [JsonPropertyName("description")]
+        public string? description { get; set; }
+    }
+}
